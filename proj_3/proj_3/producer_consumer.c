17d9723b6c6522fbd3d42c6e5a02be145a96e3e0
bGarciaMarti <bdenny@uccs.edu>
2025-04-19 13:10:41 -0600

    Changed to a FIFO method
    now part 2 works

--- proj_3/proj_3/producer_consumer.c
+++ proj_3/proj_3/producer_consumer.c
@@ -19,11 +19,14 @@
 #define PATH_SIZE 80
 #define BUFFER_SIZE 15
 
-char *sillyCh;
+char *ch;
 int count = 0;
+int in = 0, out = 0; // in and out pointrs for the circular buffer FIFO, from: Zainab Olalekan
 pthread_mutex_t mutex;
 sem_t full, empty; // semaphores
-int sillyBuffer[BUFFER_SIZE];
+char sillyBuffer[BUFFER_SIZE];
+bool done = FALSE; // flag to indicate if the producer is done, from: Zainab Olalekan
+
 pthread_t producer_thread, consumer_thread;
 pthread_attr_t attr; // set of thread attributes
 
@@ -37,10 +40,10 @@ const char * DIRECTORY_PATH = "/Users/Secondary/Desktop/CS4500/CS4500/proj_3/pro
     void getFilePath(char* finalPath, const char* directoryPath, const char* fileName);
 
     // Create a function as defined below for the producer thread which reads characters one by one from a string stored in a file, then writes sequentially these characters into a circular queue.
-    void *producer(const char* path);
+    void *producer(void * arg);
 
     // Create a function as defined below for the consumer thread which reads sequentially from the queue and prints them in the same order.
-    void *consumer(void *parameter);
+    void *consumer(void * arg);
 
 // ~~~ // MAIN // ~~~ //
 
@@ -49,8 +52,6 @@ int main(void){
     pthread_mutex_init(&mutex, NULL); //create the mutex lock
     sem_init(&full, 0, 0); // semaphore full to 0
     sem_init(&empty, 0, BUFFER_SIZE); // semaphore empty to 0
-    // pthread_attr_init(&attr); // attributes
-    
     
     // lengthArray replacement arrays to store the "finalPath" in getFilePath function
     char sillyString_filePath[PATH_SIZE] = { '\0' };
@@ -59,15 +60,13 @@ int main(void){
     getFilePath(sillyString_filePath, DIRECTORY_PATH, STRING_FILE);
 
     // we need the string from the files
-    // producer(sillyString_filePath);
-    
-    pthread_create(&producer_thread, NULL, producer(sillyString_filePath), NULL);
+    // * pass function pointer and argument seperately for producer(sillyString_filePath);
+    pthread_create(&producer_thread, NULL, producer, sillyString_filePath);
     pthread_create(&consumer_thread, NULL, consumer, NULL);
     
     // Wait until the threads are done
     pthread_join(producer_thread, NULL);
     pthread_join(consumer_thread, NULL);
-    
     
     return 0;
 } // end main
@@ -77,7 +76,8 @@ int main(void){
 bool insert_into_buffer(char item)
 {
     if (count < BUFFER_SIZE) {
-        sillyBuffer[count] = item;
+        sillyBuffer[in] = item; // buffer logic is FIFO, from: Zainab Olalekan
+        in = (in + 1) % BUFFER_SIZE;
         count++;
         return TRUE;
     } else {
@@ -90,7 +90,8 @@ bool insert_into_buffer(char item)
 bool remove_from_buffer(char *item)
 {
     if (count > 0) {
-        *item = sillyBuffer[count-1];
+        *item = sillyBuffer[out]; // buffer logic is FIFO, from: Zainab Olalekan
+        out = (out + 1) % BUFFER_SIZE;
         count--;
         return TRUE;
     } else {
@@ -100,56 +101,75 @@ bool remove_from_buffer(char *item)
 
 // 333 // USER FUNCTION: producer // 333 //
 // Create a function as defined below for the producer thread which reads characters one by one from a string stored in a file, then writes sequentially these characters into a circular queue.
-void *producer(const char* path)
-{   puts("PRODUCER BEGINNING");
+void *producer(void * arg) // buffer logic is FIFO, from: Zainab Olalekan
+{   const char *path = (const char *)arg; //cast the argument to a string, from: Zainab Olalekan
+    
+    puts("PRODUCER BEGINNING");
     FILE *inPtr; //datastream to open
     inPtr = fopen( path, "r");
     if (inPtr == NULL)
     {   printf("%s", "ERROR: File could not be opened.\n");
         exit(1);
     } else {
-        sillyCh = (char *)malloc(sizeof(char) * MAX_INPUT_BYTES);
-        if (sillyCh == NULL)
+        ch = (char *)malloc(sizeof(char) * MAX_INPUT_BYTES);
+        char sillyCh;
+        
+        if (ch == NULL)
         {   puts("ERROR: Out of memory");
             exit(1);
         }
-        pthread_mutex_lock(&mutex); // retrieve the mutex lock to produce item
-        while ((*sillyCh = fgetc(inPtr)) != EOF) //
+        
+        while ((sillyCh = fgetc(inPtr)) != EOF) //
         {
             sem_wait(&empty); // retrieve the empty lock if the buffer has space
+            pthread_mutex_lock(&mutex); // retrieve the mutex lock to produce item
             
-            if (insert_into_buffer(*sillyCh)){
-                printf("producer produced %s\n", sillyCh);
+            if (insert_into_buffer(sillyCh)){
+                printf("producer produced %c\n", sillyCh);
             } else {
                 fprintf(stderr, "WARNING: The buffer is full.\nSome items must be consumed before more production.\n");
                 break;
             }
+            pthread_mutex_unlock(&mutex);// release the mutex lock
+            sem_post(&full); // increment full
         } // end of while not the end of a file
     } // end of if else
-    pthread_mutex_unlock(&mutex);// release the mutex lock
-    sem_post(&full); // increment full
+
     fclose(inPtr); //close the file
+    
+    // from: Zainab Olalekan
+    pthread_mutex_lock(&mutex); //Signal that the producer is done
+    done = TRUE;
+    pthread_mutex_unlock(&mutex);
+    sem_post(&full); // wake up the consumer if waiting
+    
     return NULL;
 }
 
 // 444 // USER FUNCTION: consumer // 444 //
 // Create a function as defined below for the consumer thread which reads sequentially from the queue and prints them in the same order.
-void *consumer(void *parameter)
+void *consumer(void * arg)
 {   puts("CONSUMER BEGINNING");
-    pthread_mutex_lock(&mutex); // retrieve the mutex lock to consume item
+    char sillyCh;
+    
     while(TRUE)
     {
         sem_wait(&full); // retrieve the full lock to see if the buffer has an item
-            
-        if(remove_from_buffer(sillyCh)){
-            printf("consumer consumed %s\n", sillyCh);
-        } else {
-            fprintf(stderr, "ERROR: Consumer couldn't consume.\n");
+        pthread_mutex_lock(&mutex); // retrieve the mutex lock to consume item
+        
+        // check for termination, from: Zainab Olalekan
+        
+        if(!remove_from_buffer(&sillyCh) && (done)){
+            pthread_mutex_unlock(&mutex);// release the mutex lock
+            sem_post(&empty); // increment empty
             break;
+        } else {
+            printf("consumer consumed %c\n", sillyCh);
         }
-    } // end of a forever loop
+
     pthread_mutex_unlock(&mutex);// release the mutex lock
     sem_post(&empty); // increment empty
+    } // end of a forever loop
     return NULL;
 }
 
